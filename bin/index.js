2b1422324b728c537ab6e6ca70a386ba435d227b
Resuragam <1791614500@qq.com>
2023-05-23 19:48:15 +0800

    feat: add four basic commands

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,17 @@
 #! /usr/bin/env node
-console.log('auto-cli....')
-console.log(2222)
+const ora = require('ora')
+const program = require('commander')
+const inquirer = require('inquirer')
+const chalk = require('chalk')
+const download = require('download-git-repo')
+
+program
+    .version(require('../package.json').version)
+    .usage('<command> [options]')
+    .command('add', 'add a new template')
+    .command('delete', 'delete a template')
+    .command('list', 'list all the templates')
+    .command('init', 'generate a new project from a template')
+
+// 解析命令行参数
+program.parse(process.argv)
