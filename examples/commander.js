0195186a3fb452517f997913cafc358763b5ab50
Resuragam <1791614500@qq.com>
2023-05-25 20:44:14 +0800

    test: test commander

---
+++ examples/commander.js
@@ -0,0 +1,15 @@
+const program = require('commander')
+
+program
+    .version('0.0.1')
+    .command('rmdir <dir> [otherDirs...]')
+    .action(function(dir, otherDirs) {
+        console.log('rmdir %s', dir);
+        if (otherDirs) {
+            otherDirs.forEach(function(oDir) {
+                console.log('rmdir %s', oDir);
+            });
+        }
+    });
+
+program.parse(process.argv);
